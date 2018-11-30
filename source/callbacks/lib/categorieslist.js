400a20e67ae22f903543537c310fa8ec5bb2a852
Jeff Schwartz <jefftschwartz@gmail.com>
2018-11-30 13:02:58 -0500

    now converting category and tag to lowercase when used in urls

--- source/callbacks/lib/categorieslist.js
+++ source/callbacks/lib/categorieslist.js
@@ -2,7 +2,7 @@ module.exports = ($, site) => {
     site.categoriesCatalog.forEach(item => {
         $("ul.category__list").append(/* html */`
             <li class="category__list-item">
-                <a data-trio-link class="category__link" href="/blog/category/${item.category}">${item.category}</a>
+                <a data-trio-link class="category__link" href="/blog/category/${item.category.toLowerCase()}">${item.category}</a>
             </li>
         `);
     });
