c9b95c088dc9459878062709085845138ce7b96a
Jeff Schwartz <jefftschwartz@gmail.com>
2018-12-04 16:51:53 -0500

    now adding articles related by tags to the related articles list

--- source/callbacks/lib/relatedarticleslist.js
+++ source/callbacks/lib/relatedarticleslist.js
@@ -1,9 +1,17 @@
 module.exports = ($, page) => {
-    page.relatedArticlesByCategory.related.forEach(article => {
+    const getRelatedSetOfArticles = () => {
+        const articlesMap = new Map();
+        page.relatedArticlesByCategory.related.forEach(article => articlesMap.set(article.id, article));
+        page.relatedArticlesByTagFlattened.forEach(article => articlesMap.set(article.id, article));
+        return articlesMap;
+    };
+
+    const relatedArticles = getRelatedSetOfArticles();
+    relatedArticles.forEach(article => {
         $("ul.related-articles__list").append(/* html */`
                 <li class="related-articles__list-item">
                     <a data-trio-link class="related-articles__list-item-link" href="${article.url}">
-                        ${article.title} posted to "${page.relatedArticlesByCategory.category}" on ${article.articleDate} 
+                        ${article.title} posted on ${article.articleDate} 
                     </a>
                 </li>
             `);
