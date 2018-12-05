29b1aec6fe9fa86def70d641889f324f7e738f7d
Jeff Schwartz <jefftschwartz@gmail.com>
2018-12-04 21:06:54 -0500

    converting tag to lower case

--- source/callbacks/lib/articletagslist.js
+++ source/callbacks/lib/articletagslist.js
@@ -2,7 +2,7 @@ module.exports = ($, page) => {
     page.relatedArticlesByTag.forEach(item => {
         $("ul.article-tags__list").append(/* html */`
             <li class="article-tags__list-item">
-                <a data-trio-link class="article-tags__list-item-link" href="/blog/tag/${item.tag}" title="tag">${item.tag}</a>
+                <a data-trio-link class="article-tags__list-item-link" href="/blog/tag/${item.tag.toLowerCase()}" title="tag">${item.tag}</a>
             </li>
         `);
     });
