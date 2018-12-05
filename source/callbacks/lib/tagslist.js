19aad387927a000dff4dee0af658c852b0362f3f
Jeff Schwartz <jefftschwartz@gmail.com>
2018-12-05 00:36:57 -0500

    category list item links, tag list item links, related article list item links, archive list item links are now all underlined

--- source/callbacks/lib/tagslist.js
+++ source/callbacks/lib/tagslist.js
@@ -2,7 +2,7 @@ module.exports = ($, site) => {
     site.sortedTagCatalog.forEach(item => {
         $("ul.tag__list").append(/* html */`
             <li class="tag__list-item">
-                <a data-trio-link class="tag__link" href="/blog/tag/${item.tag.toLowerCase()}">${item.tag}</a>
+                <a data-trio-link class="tag__list-item-link" href="/blog/tag/${item.tag.toLowerCase()}">${item.tag}</a>
             </li>
         `);
     });
